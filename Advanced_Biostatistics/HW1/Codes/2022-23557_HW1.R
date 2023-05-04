6eb49b7a202f14434e3c09e2ced6c2f666333100
jigsaw-snu <sglee0723@snu.ac.kr>
2023-05-04 15:55:27 +0900

    file importing code added

--- Advanced_Biostatistics/HW1/Codes/2022-23557_HW1.R
+++ Advanced_Biostatistics/HW1/Codes/2022-23557_HW1.R
@@ -3,3 +3,13 @@ library(DESeq2)
 library(edgeR)
 
 
+count_data <- read.csv("../Dataset/LIHC_counts.csv", row.names = 1)
+count_data <- count_data[grepl("ENSG", rownames(count_data)),]
+colnames(count_data) <- gsub('\\.', '-', colnames(count_data))  # Genes : 60483, Samples : 424
+
+count_data <- count_data[rowSums(count_data) > 0, ]  # Genes : 56451, Samples : 424
+pseudo_count <- log2(count_data + 1)
+
+
+
+pheno_data <- read.csv("../Dataset/LIHC_phenotypes.csv", row.names = 1)
