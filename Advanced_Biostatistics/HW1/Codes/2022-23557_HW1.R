c845ac1e0d9a2fa52c4ab668cd1f2f3d86a66cfb
jigsaw-snu <sglee0723@snu.ac.kr>
2023-05-04 17:40:21 +0900

    plotting count histogram of samples

--- Advanced_Biostatistics/HW1/Codes/2022-23557_HW1.R
+++ Advanced_Biostatistics/HW1/Codes/2022-23557_HW1.R
@@ -10,6 +10,43 @@ colnames(count_data) <- gsub('\\.', '-', colnames(count_data))  # Genes : 60483,
 count_data <- count_data[rowSums(count_data) > 0, ]  # Genes : 56451, Samples : 424
 pseudo_count <- log2(count_data + 1)
 
+count_primary <- count_data[, 1]  # peek a primary tumor sample
+count_recurrent <- count_data[, 194]  # peek a recurrent tumor sample
+count_normal <- count_data[, 33]  # peek a normal tissue sample
+
+pseudo_primary <- pseudo_count[, 1]
+pseudo_recurrent <- pseudo_count[, 194]
+pseudo_normal <- pseudo_count[, 33]
+
+hist(count_primary, 
+     freq = FALSE, breaks = 30, col = "pink", 
+     xlim = c(0, 2e+06), ylim = c(0, 2e-06),
+     xlab = "gene counts", main = "Histogram of Primary Tumor")
+
+hist(count_recurrent, 
+     freq = FALSE, breaks = 30, col = "pink", 
+     xlim = c(0, 1e+06), ylim = c(0, 2e-05),
+     xlab = "gene counts", main = "Histogram of Recurrent Tumor")
+
+hist(count_normal,
+     freq = FALSE, breaks = 500, col = "pink", 
+     xlim = c(0, 1e+06), ylim = c(0, 2e-05),
+     xlab = "gene counts", main = "Histogram of Normal Tissue")
+
+hist(pseudo_primary, 
+     freq = FALSE, breaks = 50, col = "pink", 
+     xlim = c(0, 25), ylim = c(0, 1.2),
+     xlab = "log2(gene counts)", main = "Histogram of Primary Tumor")
+
+hist(pseudo_recurrent, 
+     freq = FALSE, breaks = 50, col = "pink", 
+     xlim = c(0, 25), ylim = c(0, 1.1),
+     xlab = "log2(gene counts)", main = "Histogram of Recurrent Tumor")
+
+hist(pseudo_normal, 
+     freq = FALSE, breaks = 50, col = "pink", 
+     xlim = c(0, 25), ylim = c(0, 1.2),
+     xlab = "log2(gene counts)", main = "Histogram of Normal Tissue")
 
 
 pheno_data <- read.csv("../Dataset/LIHC_phenotypes.csv", row.names = 1)
